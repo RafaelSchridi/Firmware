70f3c6f58b6a2276c3fc93680d40649ad18789e3
Rafael Schridi <r.schridi@gmail.com>
2020-03-23 11:58:22 +0100

    remove local references

--- src/modules/gpio_switching/gpio_switching.cpp
+++ src/modules/gpio_switching/gpio_switching.cpp
@@ -1,6 +1,5 @@
 #include "gpio_switching.h"
 #include <px4_config.h>
-#include "/home/rafael/dev/PX4/platforms/nuttx/NuttX/nuttx/arch/arm/src/stm32/stm32.h"
 #include "board_config.h"
 
 #include <arch/board/board.h>
